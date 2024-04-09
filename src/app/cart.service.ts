e165f8b5b86621f032f4a05222949dbffa04a8df
Chen Junjie <junjie.chen@diva.co.jp>
2024-04-09 16:31:46 +0900

    finished Mystore App

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   itemsInfo: Product[] = [];
-  constructor() { }
+  
+  constructor(private http: HttpClient) {
+
+  }
 
   addToCart(product: Product) {
     this.itemsInfo.push(product);
@@ -19,5 +23,9 @@ export class CartService {
   clearCart() {
     this.itemsInfo = [];
     return this.itemsInfo;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
